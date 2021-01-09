26d1019c1347f3730f6a6b09c31d8ad4b05d9e80
zhanglijie01 <zhanglijie01@rouchi.com>
2021-01-10 00:22:12 +0800

    add

---
+++ Algorithms/0216.reverse-linked-list/reverse-linked-list.go
@@ -0,0 +1,23 @@
+package problem0216
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		temp := head.Next
+		head.Next = prev
+		prev = head
+		head = temp
+	}
+	return prev
+}
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
